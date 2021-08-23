d0914e21562c8c933109499dcdfc1fcd4e479b02
kristt26 <kristt26@gmail.com>
2021-08-23 09:44:05 +0900

    ok

--- application/views/guest/visimisi/index.php
+++ application/views/guest/visimisi/index.php
@@ -1,3 +1,12 @@
+<div id="wisata" class="cards-2" style="padding-top: 0rem !important;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">VISI MISI DINAS PARIWISATA KOTA SORONG</div>
+                </div>
+            </div> <!-- end of row -->
+        </div>
+    </div>
 <div id="about" class="counter">
     <div class="container">
         <div class="row">
@@ -5,7 +14,7 @@
                 <div class="text-container">
                     <div class="section-title">Visi</div>
                     <p class="text-justify">Terwujudnya Kota Sorong sebagai salah satu pintu masuk wisatawan dan pusat pelayanan wisata yang bertaraf Internasional di daerah papua serta mewujudkan, peningkatan sarana dan prasarana pariwisata</p>
-                    
+
                 </div>
             </div>
             <div class="col-lg-7 col-xl-6">
